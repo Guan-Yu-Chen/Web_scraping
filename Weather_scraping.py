224848c31e2208b4730832b3f82c33b1f2aa9d0c
Guan-Yu-Chen <155933115+Guan-Yu-Chen@users.noreply.github.com>
2024-02-17 13:20:08 +0800

    Create Weather_scraping.py
    中央氣象局天氣爬蟲
    目前只有歸仁區的
    之後計畫新增其他地區
    
    把各個地區的geocode 做成 json
    新增一個list 作為會需要爬的地區
    要加地區或刪除地區就用指令修改list

---
+++ Weather_scraping.py
@@ -0,0 +1,30 @@
+import requests as req
+import json
+from bs4 import BeautifulSoup as bs
+
+author_code = ""
+
+url_tainan_1week = f"https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/F-D0047-079?Authorization={author_code}&downloadType=WEB&format=JSON"
+
+res = req.get(url_tainan_1week)
+
+obj = json.loads(res.text)
+
+WeatherInfo = obj["cwaopendata"]["dataset"]["locations"]
+
+time = obj["cwaopendata"]["sent"][0:10]
+
+for area in WeatherInfo["location"]:
+    if area["geocode"] == "67000280":
+        district = WeatherInfo["locationsName"] + area["locationName"]
+        MaxT = area["weatherElement"][3]["time"][0]["elementValue"]["value"]
+        MinT = area["weatherElement"][4]["time"][0]["elementValue"]["value"]
+        ProbOfRain = area["weatherElement"][9]["time"][0]["elementValue"]["value"]
+        weather = area["weatherElement"][12]["time"][0]["elementValue"][0]["value"]
+
+
+print(f"地區 : {district}")
+print(f"時間 : {time}")
+print(f"溫度 : {MinT}°C - {MaxT}°C") #最高最低
+print(f"天氣 : {weather}")
+print(f"降雨機率 : {ProbOfRain} %")
